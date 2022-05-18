458cd628080cad2553f63d93eda1096526aaac88
Gabriel Ahrens <gab.ahrens@gmail.com>
2022-05-18 22:00:40 +0200

    Added search function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import {Routes, Route} from "react-router-dom";
 import { Carrossel } from "./Components/Carrosel";
 import {Forms} from "./Components/Forms"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { SearchResults } from "./Components/SearchPage";
+import { useState } from "react";
+import { NavBar } from "./Components/TesteBar";
 
 function App() {
+  const [test, setTest] = useState()
+ 
   return (
     <div className="App">
+      <NavBar setTest={setTest} />
     <Routes>
       <Route path="/" element ={<Forms/>}/>
+      <Route path="/search" element ={<SearchResults test={test} />}/>
     </Routes>
       <Carrossel />
     </div>
---
+++ src/Components/TesteBar/index.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+
+export function NavBar(props) {
+
+    function handleChange(e) {
+        props.setTest((e.target.value).toLocaleLowerCase())
+    }
+
+    return (
+        <div>
+            <input type="search" placeholder="Busque por um restaurante por nome ou cidade!" onChange={handleChange}/>
+            <Link to={"/search"}><button>Buscar</button></Link> 
+        </div>
+    )
+}
+    
