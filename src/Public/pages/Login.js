3adda12d916a858c74311cb403e3d35341de17c3
Hudson Arpini <hudson_arpini@hotmail.com>
2022-05-19 15:50:17 -0300

    login logic

--- src/Public/pages/Login.js
+++ src/Public/pages/Login.js
@@ -1,35 +1,52 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { api } from "../../API/Api"
 export function Login(){
     const navigate = useNavigate();
-    const [user, setUser] = useState({});
+
+    const [profile, setProfile] = useState('user');
+
     const [form, setForm] = useState({
         email: "",
         password: "",
     });
+
     function handleChange(event) {
         setForm({ ...form, [event.target.name]: event.target.value });
         console.log("HUNT",form);
     }
 
-    useEffect(() => {
-        async function fetchData(){
-            const response = await api.get('restaurants/all-restaurants');
-            setUser(response.data);
-        }
-        fetchData();
-    },[])
-
-    useEffect(() => {
-        async function fetchData(){
-            const response = await api.post('user/signup');
-            setUser(response.data);
-        }
-        fetchData();
-    },[])
+    function userClick(){
+        setProfile('user')
     
+    }
+
+    function restaurantClick(){
+        setProfile('restaurant')
+       
+    }
+
+    async function handleSubmit(event){
+        event.preventDefault()
+        
+        if(profile === 'user'){
+            try{
+                await api.post("/user/login", form)
+                navigate("/homeuser")
+
+            }catch(error){console.log(error)}
+        } 
+
+        if(profile === 'restaurant'){
+            try{
+                await api.post("/restaurant/login", form)
+                navigate("/restaurantuser")
+
+            }catch(error){console.log(error)}
+        }
+    }
+   
     
     return(
         <>
@@ -52,9 +69,16 @@ export function Login(){
                         onChange={handleChange}>
                     </input>
                 </span>
-                <input type="radio"></input>
-                <button type="submit">Entrar</button>
+               
+                <input type="radio" name="type" onClick={userClick} placeholder="usuario"/>  Sou usuário
+            
+               
+                <input type="radio" name="type" onClick={restaurantClick} placeholder="usuario" /> <br/> Sou restaurante
+                
+                <button type="submit" onClick={handleSubmit}>Entrar</button>
             </form>
+
+            
             <p><Link to="/signup/user">Cadastrar</Link></p>
             <Link to="/singip/restaurant">Cadastrar restaurante</Link>
 
